e3ceb2555ae42e7654e16b48b7d4f2d9282cbc44
mohithingorani <hingoranimohit@gmail.com>
2020-05-16 02:20:22 -0700

    cylinder mesh distortion experiment

--- public/horizon.js
+++ public/horizon.js
@@ -3,7 +3,7 @@
 // essentials
 var gui = new dat.GUI();
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1, 1000000 );
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 100000 );
 var renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
 const loader = new THREE.TextureLoader();
 var spriteMap = loader.load("assets/circle-64.png" );
@@ -40,16 +40,11 @@ var controls = new function() {
     this.sunSize = 1;
     this.sunInclination = 0;
 
-    this.enableStars = true;
-    this.starColor = 0xffffff;
-    this.starSize = 50;
-    this.starDensity = 10000;
-
     this.fogColor = 0xeb2df7;
     this.fogDensity = 2; //  dividing by 100000
 
-    this.terrainEmissiveColor = 0x000000;
-    this.terrainBaseColor = 0x000000;
+    this.terrainEmissiveColor = 0x00b9e1;
+    this.terrainBaseColor = 0x00c3e1;
     this.wireframeEmissiveColor = 0x00aaff;
     this.wireframeColor = 0x000000;
     this.showWireframe = true;
@@ -58,7 +53,7 @@ var controls = new function() {
     this.elevate = 1;
     this.baseHeight = 600;
 
-    this.enableDynamic = true;
+    this.enableDynamic = false;
     this.amplitude = 150;
     this.frequency = 0.5;
 };
@@ -74,12 +69,6 @@ sunFolder.addColor(controls, 'sunLightColor').name('Sun Reflect Color');
 sunFolder.add(controls, 'sunSize',0.001,5).name('Sun Scale');
 sunFolder.add(controls, 'sunInclination',-10,10).name('Sun Inclination');
 
-var starFolder = gui.addFolder('Stars');
-starFolder.add(controls, 'enableStars').name('Enable Stars');
-starFolder.addColor(controls, 'starColor').name('Star Color');
-starFolder.add(controls, 'starSize',1,250).name('Star Size');
-// starFolder.add(controls, 'starDensity',100,10000).name('Star Density');
-
 var fogFolder = gui.addFolder('Fog');
 fogFolder.addColor(controls, 'fogColor').name('Fog Color');
 fogFolder.add(controls, 'fogDensity',0,5).name('Fog Density');
@@ -94,7 +83,7 @@ meshFolder.addColor(controls, 'wireframeEmissiveColor').name('Wire Emissive Colo
 // meshFolder.addColor(controls, 'wireframeColor').name('Wireframe Color');
 
 meshFolder.add(controls, 'pathWidth',0,15).name('Path Width');
-meshFolder.add(controls, 'elevate', 0, 5).name('Hill Height');
+meshFolder.add(controls, 'elevate', 0, 1).name('Hill Height');
 meshFolder.add(controls, 'baseHeight', 0, 2000).name('Drop Plane');
 meshFolder.open();
 
@@ -105,41 +94,14 @@ utilityFolder.add(controls, 'frequency', 0, 2).name('Frequency');
 
 // intialize three
 
-var segmentLength = 200;
+var segmentLength = 100;
 var w = 5000;
-var h = 10000;
+var h = 5000;
 var ws = w/segmentLength;
 var hs = h/segmentLength;
 var sunSizeBase = 4000;
 var sunPositionMultiplier = 1500;
 var sunZPosition = -10000;
-
-
-
-// initialize star
-var particles = 1000;
-var positions = [];
-var n = 100000, n2 = n / 2;
-var starGeometry = new THREE.BufferGeometry();
-
-for ( var i = 0; i < particles; i ++ ) {
-    var x = Math.random() * n - n2;
-    var y = Math.random() * n - n2;
-    var z = -11000;
-
-    positions.push( x, y, z );
-}
-
-starGeometry.addAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
-var starMaterial = new THREE.PointsMaterial( { 
-    color: 0xffffff,
-    depthTest: true,
-    fog: false,
-    map: spriteMap,
-    size: 100,
-    sizeAttentuation: false,
-} );
-var stars = new THREE.Points( starGeometry, starMaterial );
 
 
 
@@ -161,12 +123,19 @@ var terrainGeometry = new THREE.PlaneGeometry( w,h,ws-1,hs-1); // - 1 since it u
 terrainGeometry.rotateX( - Math.PI / 2 );
 terrainGeometry.translate(-(w/ws)/2,0, 0);
 
-var terrainMaterial = new THREE.MeshPhongMaterial( { color: controls.terrainBaseColor, emissive: controls.terrainEmissiveColor, side: THREE.DoubleSide, transparent: true } ) 
+var terrainMaterial = new THREE.MeshPhongMaterial( { color: controls.terrainBaseColor, emissive: controls.terrainEmissiveColor, side: THREE.DoubleSide, transparent: true, wireframe: false } ) 
 var wireframeMaterial = new THREE.MeshPhongMaterial( { color: controls.wireframeColor, emissive: controls.wireframeEmissiveColor, wireframe: true } )
 
 var baseTerrainMesh = new THREE.Mesh( terrainGeometry, terrainMaterial );
 var wireframeTerrainMesh = new THREE.Mesh( terrainGeometry, wireframeMaterial );
 
+
+var cylinderTerrainMaterial = new THREE.MeshBasicMaterial( { color: controls.terrainBaseColor, side: THREE.DoubleSide, transparent: true, opacity: 1.0, wireframe: true } ) 
+
+var cylinderTerrainGeometry = new THREE.CylinderGeometry(20,20,200,50,50,true); // - 1 since it uses segments - keeps the math straight // 300,300,2000,100,100
+var cylinderTerrainMesh = new THREE.Mesh( cylinderTerrainGeometry, cylinderTerrainMaterial );
+
+cylinderTerrainMesh.rotateX( - Math.PI / 2 );
 
 // generate terrain
 
@@ -185,6 +154,7 @@ function generateHeight( size ) {
             var y = (parseInt(i/ws))/hs;
             var z = perlin.noise(x,y, i * startMillis/1000); //startMillis helps keep things random on each initialize
             data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z )) * quality;
+            // data.normalize().multiplyScalar(1 + 0.3 * noise.perlin3(p.x * k, p.y * k, p.z * k));
         }
         quality *= 5;
     }
@@ -212,15 +182,15 @@ animate();
 
 function init(){
 
-    camera.position.set(0,0,4000);
+    camera.position.set(0,0,1);
     
     trackBallControls.rotateSpeed = 4;
     trackBallControls.zoomSpeed = 1;
 
-    scene.add(wireframeTerrainMesh);
-    scene.add( baseTerrainMesh );
-    scene.add( sunLight );
-    scene.add( stars );
+    // scene.add( wireframeTerrainMesh);
+    // scene.add( baseTerrainMesh );
+    // scene.add( sunLight );
+    scene.add( cylinderTerrainMesh );
 }
 
 // threejs animate function
@@ -237,10 +207,6 @@ function animate() {
     sunLight.scale.x = controls.sunSize;
     sunLight.scale.y = controls.sunSize;
 
-    stars.visible = controls.enableStars;
-    starMaterial.color.setHex(controls.starColor);
-    starMaterial.setValues({size: controls.starSize});
-
     terrainMaterial.color.setHex(controls.terrainBaseColor);
     terrainMaterial.emissive.setHex(controls.terrainEmissiveColor);
     
@@ -253,8 +219,35 @@ function animate() {
         terrainGeometry.vertices[i].y = newHeight[i]  * attenuate(i) - controls.baseHeight ;
     }
 
+    //generate cylinder terrain
+    var modulate = clock.getElapsedTime() * controls.frequency;
+    
+
+    var k = 0.5;
+    for ( var i = 0; i < cylinderTerrainMesh.geometry.vertices.length; i ++ ) {
+        var dir = new THREE.Vector3();
+        // dir.subVectors( p, sliceCenter ).normalize();
+        // var noiseX = (1 +  0.2 * perlin.noise(i/3, i%5 , i%17));
+        
+        var noiseX = (1 + k * perlin.noise(i%2, i%5, i/17));
+        var noiseZ = (1 + k * perlin.noise(i/13, i%11 , i%3));
+        // console.log( noiseX,noiseZ );
+
+        var x = cylinderTerrainMesh.geometry.vertices[i].x * noiseX;
+        var y = cylinderTerrainMesh.geometry.vertices[i].y;
+        var z = cylinderTerrainMesh.geometry.vertices[i].z * noiseZ;
+        cylinderTerrainMesh.geometry.vertices[i] = new THREE.Vector3(x,y,z);
+        // p.normalize().multiplyScalar(1 + 0.1 * perlin.noise(i/50, i%50 , i%100));
+        
+    }
+    
+    cylinderTerrainMesh.geometry.verticesNeedUpdate = true;
+    cylinderTerrainMesh.geometry.computeVertexNormals();
+    cylinderTerrainMesh.geometry.computeFaceNormals();
+
     terrainGeometry.verticesNeedUpdate = true;
     wireframeTerrainMesh.position.set(0,1,0);
+    
     trackBallControls.update();
     render();
 };
@@ -311,50 +304,3 @@ var saveFile = function (strData, filename) {
         location.replace(uri);
     }
 }
-
-
-
-
-// var quadGeoemetry = terrainGeometry.clone();
-
-// var lineSegementMeshes = [];
-// var lineGeometries  = [];
-// var lineGroup = new THREE.Group();
-// var lineMaterial = new THREE.LineBasicMaterial({color: '#00ffff', linewidth: 1});
-
-// for (var i = 0; i < terrainGeometry.vertices.length; i+=ws) {
-    //     var tempGeometry = new THREE.Geometry();
-    //     var tempVertexArray = terrainGeometry.vertices.slice(i, i + ws);
-    //     tempGeometry.vertices = tempVertexArray;
-    //     tempGeometry.name = i;
-    
-    //     lineGeometries.push(tempGeometry);
-    // }
-    
-    // console.log(lineGeometries);
-    
-    // for ( var i = 0 ; i < lineGeometries.length; i++ ){
-        //     lineSegementMeshes.push(new THREE.Line( lineGeometries[i], lineMaterial ))
-        //     lineSegementMeshes.position =  new THREE.Vector3(100,100,100);
-        //     lineGroup.add(lineSegementMeshes[i]);
-        // }
-        
-        
-        // terrainGeometry.verticesNeedUpdate = true;
-        // console.log('vertex',vertex);
-        // camera.lookAt(new THREE.Vector3(0,0,0));
-
-
-            // // generateHeight();
-    // var time = clock.getElapsedTime();
-    // for (let i = 0 ; i < terrainGeometry.vertices.length ; i++ ) {
-    //     var x = i % ws
-    //     var y = (parseInt(i/ws))/hs;
-    //     var z = perlin.noise(x,y, i * currentMillis/10000);
-    //     var localNoise = Math.abs(perlin.noise(x,y,z));
-    //     // var randomNoise = controls.amplitude * perlin.noise(i,x,y * Math.cos(controls.speed*time));
-    //     // var randomNoise = controls.amplitude * (perlin.noise(Math.cos(time * y * controls.speed),(time * controls.speed),Math.sin(time * x * controls.speed)))
-
-    //     terrainGeometry.vertices[i].y = (localNoise) * attenuate(i) - controls.baseHeight;
-    //     // terrainGeometry.vertices[i].y = (Math.abs(perlin.noise(x,y,i)) + controls.amplitude * Math.sin((time+i)/controls.speed)) * attenuate(i) - controls.baseHeight;
-    // }
