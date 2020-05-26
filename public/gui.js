cd10f5455e9d7e10e0d2236ffb4a197cd7d8a9be
mohithingorani <hingoranimohit@gmail.com>
2020-05-26 02:10:33 -0700

    Add ambient light controls, reorder UI & default to iceland

--- public/gui.js
+++ public/gui.js
@@ -8,23 +8,34 @@ var currentTime = date.getTime();
 var controls = new function() {
     this.takeImage = function(){ saveAsImage() };
     this.researchLink = function() {window.open('https://www.youtube.com/watch?v=EtI7f3Rwqkw')};
-    this.backgroundColor = 0x282555;
-    this.form = 'Plane';
-    this.wireframe = false;
-    this.formColor = 0x5f1769;
-    this.emissiveColor = 0x1a2fac;
+    this.backgroundColor = 0x006278;
+    this.ambientLightColor = 0x404040;
+    this.ambientLightIntensity = 1;
     this.fogDensity = 0;
     this.fogColor = 0xe691e6;
 
-    this.dynamic = true;
+    this.form = 'Plane';
+    this.wireframe = false;
+    this.formColor = 0xffffff;
+    this.emissiveColor = 0x001625;
+    this.roughness = 2;
+
+    this.dynamic = false;
     this.amplitude = 25;
     this.frequency = 0.5;
-    this.distortion = 600;
+    this.distortion = 20;
 };
 
 var general = gui.addFolder('Horizon Generator');
 general.add(controls, 'takeImage').name('Take Screenshot');
 general.addColor(controls, 'backgroundColor').name('Background');
+
+general.addColor(controls, 'ambientLightColor').name('Ambient Light');
+general.add(controls, 'ambientLightIntensity', 0, 5).name('Ambient Intensity');
+
+general.add(controls, 'fogDensity', 0, 100).name('Fog Density');
+general.addColor(controls, 'fogColor').name('Fog Color');
+
 general.close();
 
 var f0 = gui.addFolder('Form & Color');
@@ -32,14 +43,15 @@ f0.add(controls, 'form', [ 'Cylinder', 'Plane']);
 f0.add(controls, 'wireframe').name('Show wireframe');
 f0.addColor(controls, 'formColor').name('Form Color');
 f0.addColor(controls, 'emissiveColor').name('Emissive Color');
-f0.add(controls, 'fogDensity', 0, 100).name('Fog Density');
-f0.addColor(controls, 'fogColor').name('Fog Color');
+f0.add(controls, 'roughness', 0,5).name('Terrain Roughness');
+
 f0.open();
 
-var f1 = gui.addFolder('Cylinder');
+var f1 = gui.addFolder('Dynamic');
+f1.add(controls, 'dynamic').name('Dynamic');
 f1.add(controls, 'amplitude', 0, 100).name('Amplitude');
 f1.add(controls, 'frequency', 0, 1).name('Frequency');
-f1.add(controls, 'distortion', 1, 1000).name('Distortion');
+f1.add(controls, 'distortion', 1, 100).name('Distortion');
 f1.open();
 
 
--- public/horizon.js
+++ public/horizon.js
@@ -2,7 +2,7 @@
 
 // essentials
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 120, window.innerWidth/window.innerHeight, 0.1, 10000 );
+var camera = new THREE.PerspectiveCamera( 90, window.innerWidth/window.innerHeight, 0.1, 10000 );
 var renderer = new THREE.WebGLRenderer({ antialias: true, preserveDrawingBuffer: true });
 const loader = new THREE.TextureLoader();
 var perlin = new THREE.ImprovedNoise();
@@ -11,24 +11,37 @@ var strDownloadMime = "image/octet-stream";
 
 // set time and clocks
 var date = new Date();
-var currentTime = date.getTime();
+var startMillis = date.getMilliseconds();
+// var currentTime = date.getTime();
+var clock = new THREE.Clock();
+clock.start();
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.gammaInput = true;
 renderer.gammaOutput = true;
-// renderer.toneMapping = THREE.ReinhardToneMapping;
-// renderer.toneMapping = THREE.CineonToneMapping
 
 document.body.appendChild( renderer.domElement );
 var trackBallControls = new THREE.TrackballControls( camera, renderer.domElement );
 
+// variables
+var planeWidth = 1200;
+var planeHeight = 1200;
+var planeWidthSegments = 63; // 128 -1 since the vertex is one more.
+var planeHeightSegments = 63;
+
+var cylinderRadius1 = 30;
+var cylinderRadius2 = 30;
+var cylinderHeight = 600;
+var cylinderRadiusSegments = 64;
+var cylinderHeightSegements = 120;
+
 // intialize three
-var sphere = new THREE.SphereBufferGeometry( 1, 16, 8 );
-var pointLight = new THREE.PointLight( 0xEE77DD, 2, 0 ,2 );
-pointLight.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x111111 } ) ) );
+var sphere = new THREE.SphereBufferGeometry( 16, 16, 16 );
+var pointLight = new THREE.PointLight( 0xa0a0a0, 2, 0 ,2 );
+pointLight.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xdddddd } ) ) );
 scene.add( pointLight );
 
-var ambientLight = new THREE.AmbientLight( 0x606060 ); // soft white light
+var ambientLight = new THREE.AmbientLight( controls.ambientLightColor, controls.ambientLightIntensity ); // soft white light
 scene.add( ambientLight );
 
 // var spotLight = new THREE.SpotLight( 0x770000, 2, 1000);
@@ -43,7 +56,7 @@ var distantFog = new THREE.FogExp2( controls.fogColor, controls.fogDensity/1000 
 //  cylinder geometry
 
 var cylinderTerrainMaterial = new THREE.MeshLambertMaterial( { color: controls.formColor, emissive: controls.emissiveColor,  side: THREE.DoubleSide, transparent: true, opacity: 1.0, wireframe: false } );
-var cylinderTerrainGeometry = new THREE.CylinderGeometry(30,30,600,64,120,true); // - 1 since it uses segments - keeps the math straight // 300,300,2000,100,100
+var cylinderTerrainGeometry = new THREE.CylinderGeometry(cylinderRadius1,cylinderRadius2,cylinderHeight,cylinderRadiusSegments,cylinderHeightSegements,true); // - 1 since it uses segments - keeps the math straight // 300,300,2000,100,100
 // var cylinderTerrainGeometry = new THREE.CylinderBufferGeometry( 30, 30, 600, 64,120, true );
 var cylinderTerrainMesh = new THREE.Mesh( cylinderTerrainGeometry, cylinderTerrainMaterial );
 cylinderTerrainMesh.rotateX( - Math.PI / 2 );
@@ -52,7 +65,7 @@ var backupGeometry = cylinderTerrainGeometry.clone();
 
 //  plane geometry
 
-var planeTerrainGeometry = new THREE.PlaneBufferGeometry( 600, 600, 64, 64);
+var planeTerrainGeometry = new THREE.PlaneBufferGeometry(planeWidth, planeHeight, planeWidthSegments, planeHeightSegments);
 planeTerrainGeometry.rotateX( - Math.PI / 2 );
 
 var position = planeTerrainGeometry.attributes.position;
@@ -80,6 +93,9 @@ function animate() {
     requestAnimationFrame(animate);
     trackBallControls.update();
 
+    ambientLight.color.set(controls.ambientLightColor);
+    ambientLight.intensity = controls.ambientLightIntensity;
+    
     distantFog.color.set(controls.fogColor); // not using Hex on purpose
     distantFog.density = controls.fogDensity/10000;
 
@@ -102,19 +118,27 @@ function animate() {
 
     // animate plane
     if ( planeTerrainMesh.visible == true ) {
-        position = planeTerrainGeometry.attributes.position;
-        for ( var i = 0; i < position.count; i ++ ) {
-            var CNFrequency = Date.now()/500 * controls.frequency;
-            var x = Math.sin(CNFrequency + i/(controls.distortion));
-            var y = Math.cos(CNFrequency + i%(controls.distortion));
-            var z = 1 - x * y;
-            var perlinNoise = controls.frequency * perlin.noise(x, y, z);
-            // var height = 10 * Math.sin( i % 5 + ( time2 + i ) / 7 );
+        // position = planeTerrainGeometry.attributes.position;
+        // for ( var i = 0; i < position.count; i ++ ) {
+        //     var CNFrequency = Date.now()/500 * controls.frequency;
+        //     var x = Math.sin(CNFrequency + i/(controls.distortion));
+        //     var y = Math.cos(CNFrequency + i%(controls.distortion));
+        //     var z = 1 - x * y;
+        //     var perlinNoise = controls.frequency * perlin.noise(x, y, z);
+        //     // var height = 10 * Math.sin( i % 5 + ( time2 + i ) / 7 );
 
-            position.setY( i, controls.amplitude * ( 1 + perlinNoise ) );
+        //     position.setY( i, controls.amplitude * ( perlinNoise ) );
+        // }
+
+        var data = generateTerrain( planeWidthSegments+1 , planeHeightSegments+1)
+        var vertices = planeTerrainGeometry.attributes.position.array;
+
+        for ( var i = 0, j = 0, l = vertices.length; i < l; i ++, j += 3 ) {
+            vertices[j + 1] = data[i];
         }
+
         planeTerrainGeometry.translate(0,-50,0);
-        position.needsUpdate = true;
+        vertices.needsUpdate = true;
         planeTerrainGeometry.computeBoundingSphere();
         planeTerrainGeometry.computeVertexNormals();
         planeTerrainGeometry.computeFaceNormals();
@@ -132,7 +156,8 @@ function animate() {
     
             cylinderTerrainGeometry.vertices[i].x = backupGeometry.vertices[i].x * (1 + perlinNoise);
             cylinderTerrainGeometry.vertices[i].z = backupGeometry.vertices[i].z * (1 + perlinNoise);
-        }    
+        }
+        
         cylinderTerrainGeometry.verticesNeedUpdate = true;
         cylinderTerrainGeometry.computeVertexNormals();
         cylinderTerrainGeometry.computeFaceNormals();
@@ -152,6 +177,34 @@ function render() {
     scene.background = new THREE.Color(controls.backgroundColor);
 
     renderer.render( scene, camera );
+}
+
+
+function generateTerrain( ws, hs  ) {
+    var size = ws * hs
+    var data = new Uint8Array( size ), quality = 1;
+    for ( var j = 0; j < 5; j ++ ) {
+        for ( var i = 0; i < size; i ++ ) {
+            var x = i % ws + controls.distortion * startMillis * 0.0001;
+            var y = (parseInt(i/ws))/hs + controls.distortion * startMillis * 0.0001;
+            var z = perlin.noise(x,y,i * startMillis/1000); //startMillis helps keep things random on each initialize
+            data[ i ] += controls.roughness * quality * Math.abs( perlin.noise( x/ quality, y/quality,z ));
+        }
+        quality *= 3;
+    }
+
+    if(controls.dynamic) {
+        // var modulate = clock.getElapsedTime() * controls.frequency ;
+        var modulate = Date.now()* 0.000001 * controls.frequency;
+        var modulateFactor = 10;
+        for ( var i = 0; i < size; i ++ ) {
+            var x = i % ws
+            var y = (parseInt(i/ws))/hs;
+            var z = perlin.noise(i * modulate , x, y);
+            data[ i ] += controls.amplitude * Math.abs( perlin.noise( modulateFactor * Math.sin(modulate), modulateFactor * Math.cos(modulate), z )) ;
+        }
+    }
+    return data;
 }
 
 window.addEventListener( 'resize', function () {
